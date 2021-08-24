b7773f20656409f6718830e6e584dcce1effbee7
Kyumluvamericano <59195630+KyumKyum@users.noreply.github.com>
2021-08-24 22:51:54 +0900

    Create 15828.cpp

---
+++ 15828.cpp
@@ -0,0 +1,40 @@
+//백준 15828: Router
+//분류: 큐 (Queue)
+//https://www.acmicpc.net/problem/15828
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    
+    int size, input = 0;
+    
+    cin >> size;
+    
+    queue<int> q;
+    
+    while(1){
+        cin >> input;
+        
+        if(input <= -1){
+            break;
+        }else if(input == 0 && !q.empty()){
+            q.pop();
+        }else if (input > 0 && q.size() < size){ // Normal Input
+            q.emplace(input);
+        }
+    }
+    
+    if(q.empty()){
+        cout << "empty";
+    }else{
+        while(!q.empty()){
+            cout << q.front() << " ";
+            q.pop();
+        }
+    }
+    
+   return 0;
+}
