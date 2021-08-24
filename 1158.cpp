d98cf1873b438cf7ae8146a057ecd718a2d7462e
Kyumluvamericano <59195630+KyumKyum@users.noreply.github.com>
2021-08-24 22:47:53 +0900

    Create 1158.cpp

---
+++ 1158.cpp
@@ -0,0 +1,45 @@
+//백준 1158: 요세푸스 문제
+//분류: 큐(Queue)
+//https://www.acmicpc.net/problem/1158
+
+#include<iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int people, elim, cnt = 0;
+    queue<int> q;
+    
+    cin >> people >> elim;
+    
+    for (int i = 0; i < people; i++){
+        
+        q.emplace(i+1);
+    }
+        
+    cout << "<";
+        
+    while(!(q.empty())){
+        cnt++;
+        
+        if(cnt % elim == 0){//generating Josepus' matrix
+            cout << q.front();
+            q.pop();
+            if(!(q.empty())){
+                cout<<", ";
+            }
+        }else{
+            int container = q.front();
+            q.pop();
+            //cout << "container: " << container;
+            q.emplace(container);
+        }
+    }
+    
+    cout << ">";
+    
+
+    return 0;
+}
